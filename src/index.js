faed75aae0c06acb0cab37adc54e3979c0a03116
martinbogado <martinbogado@live.com.ar>
2021-09-13 10:18:36 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import axios from "axios";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
